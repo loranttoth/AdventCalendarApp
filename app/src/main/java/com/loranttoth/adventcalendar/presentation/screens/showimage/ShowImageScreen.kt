bf8e5d8e6796a04fcd70fdd09a280289f9052c33
Lorant Toth <tothlorant76@gmail.com>
2023-12-06 01:52:23 +0100

    image contentscale in landscape view

--- app/src/main/java/com/loranttoth/adventcalendar/presentation/screens/showimage/ShowImageScreen.kt
+++ app/src/main/java/com/loranttoth/adventcalendar/presentation/screens/showimage/ShowImageScreen.kt
@@ -1,11 +1,13 @@
 package com.loranttoth.adventcalendar.presentation.screens.showimage
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import coil.ImageLoader
 import coil.compose.AsyncImage
 import com.loranttoth.adventcalendar.presentation.screens.main.MainScreenViewModel
@@ -17,18 +19,19 @@ fun ShowImageScreen(
     viewModel: MainScreenViewModel,
     imageLoader: ImageLoader
 ) {
-        Column(
-            modifier = Modifier.fillMaxSize()
+    val contScale = if (LocalConfiguration.current.orientation == Configuration.ORIENTATION_PORTRAIT) {ContentScale.FillWidth} else {ContentScale.FillHeight}
+    Column(
+        modifier = Modifier.fillMaxSize()
 
-        ) {
-            viewModel.actImageItem?.value?.let {
-                AsyncImage(
-                    model = viewModel.actImageItem!!.value!!.sourceUrl,
-                    imageLoader = imageLoader,
-                    modifier = Modifier.fillMaxSize(),
-                    contentScale = ContentScale.FillWidth,
-                    contentDescription = null,
-                )
-            }
+    ) {
+        viewModel.actImageItem?.value?.let {
+            AsyncImage(
+                model = viewModel.actImageItem!!.value!!.sourceUrl,
+                imageLoader = imageLoader,
+                modifier = Modifier.fillMaxSize(),
+                contentScale = contScale,
+                contentDescription = null,
+            )
         }
+    }
 }
